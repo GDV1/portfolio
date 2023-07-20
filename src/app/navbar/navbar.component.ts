0033834def30a34d0f71bc7cf2dc32d9c2b2f272
Gabriel Dias <gabriel.dias@solutis.com.br>
2023-07-19 23:14:47 -0300

    layout mobile 320px

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  show = false;
+
+  constructor() {}
+
+  clicked() {
+    this.show = !this.show;
+  }
+
+  ngOnInit() {
+    
+  }
 
 }
